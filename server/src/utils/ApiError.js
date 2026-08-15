38aa39255825bc4326551bb658f315de9c8bc88d
ashishsps20 <ashishsps20@gmail.com>
2026-08-15 18:58:05 +0530

    clean up

--- server/src/utils/ApiError.js
+++
@@ -1,9 +0,0 @@
-export const PLATFORM_ROLES = ['ADMIN', 'PROJECT_MANAGER', 'DEVELOPER'];
-
-export class ApiError extends Error {
-  constructor(statusCode, message) {
-    super(message);
-    this.statusCode = statusCode;
-    this.isOperational = true;
-  }
-}
